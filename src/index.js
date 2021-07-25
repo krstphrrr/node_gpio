2f7ac5b8b2bd781ea81e234a8d2e947c5704bf33
Kris! <42949874+krstphrrr@users.noreply.github.com>
2021-07-24 19:26:17 -0600

    close gpio after function fires

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ app.get('/', (req,res)=>{
     console.log("cabrona no")
     lg.gpioWrite(h, LED, 0)
     res.send("cabrona no")
+    
   } else {
     console.log("cabrona si")
     lg.gpioWrite(h, LED, 1)
@@ -29,7 +30,7 @@ app.get('/', (req,res)=>{
   // h = lg.gpiochipOpen(0)
   // lg.gpioClaimOutput(h,LED)
 
-
+  lg.gpiochip_close(h)
 })
 
 app.listen(port,()=>{
