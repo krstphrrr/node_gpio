1486ca49778641bdd03fa21f87b250b614a16781
Kris! <42949874+krstphrrr@users.noreply.github.com>
2021-07-30 23:17:06 -0600

    changed all routes to post routes

--- src/index.js
+++ src/index.js
@@ -1,155 +1,72 @@
 const express = require('express')
+const cors = require('cors')
 const lg = require('lgpio')
 const visitCount = require('express-visitor-counter')
+const {ledPinout, iterLED, turnItOn, turnItOff} = require('./utils')
 const app = express()
 const port = 3000
-const RED = 23
-const YELLOW = 20
-const GREEN = 21
-let swtch = 0
+
 counters = {}
-let count
+
+exports.status = {"red":false,"yellow":false,"green":false}
+
+app.use(express.json());
+app.use(cors())
 
 app.use(visitCount({hook:counterId => counters[counterId] = (counters[counterId] || 0)+1}))
 app.get('/', (req,res)=>{
   res.status(200).send("please visit endpoints: '/red', '/yellow' or '/green'.")
 })
 
-app.get('/red',(req,res)=>{
-  h = lg.gpiochipOpen(0)
-  
-  lg.gpioClaimOutput(h,RED)
-  switch(swtch){
-    case 0:
-      swtch = 1
-      break 
-    case 1:
-      swtch = 0 
-      break 
-  }
-  if(swtch===0){
-    lg.gpioWrite(h, RED, 0)
-    for(let [key,value] of Object.entries(counters)){
-      count = value
-    }
-    console.log(count)
-    res.send(`<b>sam visitor:${count}; LED off</b>`)
-
-  } else {
-    lg.gpioWrite(h, RED, 1)
-    for(let [key,value] of Object.entries(counters)){
-      count = value
-    }
-    console.log(count)
-    res.status(200).send(`<b>sam visitor:${count}; LED on</b>`)
+app.post('/red',(req,res)=>{
+  console.log(req.body)
+  switch(req.body["red"]){
+    case true:
+      turnItOn(ledPinout['red'])
+      status['red'] = !status['red']
+      break
+    case false:
+      turnItOff(ledPinout['red'])
+      status["red"] = !status['red']
   }
 
-  lg.gpioFree(h,RED)
-  lg.gpiochipClose(h)
 })
 
-app.get('/yellow',(req,res)=>{
-  h = lg.gpiochipOpen(0)
-  
-  lg.gpioClaimOutput(h,YELLOW)
-  switch(swtch){
-    case 0:
-      swtch = 1
-      break 
-    case 1:
-      swtch = 0 
-      break 
+app.post('/yellow',(req,res)=>{
+  console.log(req.body)
+  switch(req.body["yellow"]){
+    case true:
+      turnItOn(ledPinout['yellow'])
+      status['yellow'] = !status['yellow']
+      break
+    case false:
+      turnItOff(ledPinout['yellow'])
+      status["yellow"] = !status['yellow']
   }
-  if(swtch===0){
-    lg.gpioWrite(h, YELLOW, 0)
-    for(let [key,value] of Object.entries(counters)){
-      count = value
-    }
-    console.log(count)
-    res.send(`<b>sam visitor:${count}; LED off</b>`)
-
-  } else {
-    lg.gpioWrite(h, YELLOW, 1)
-    for(let [key,value] of Object.entries(counters)){
-      count = value
-    }
-    console.log(count)
-    res.status(200).send(`<b>sam visitor:${count}; LED on</b>`)
-  }
-
-  lg.gpioFree(h,YELLOW)
-  lg.gpiochipClose(h)
 })
 
-app.get('/green',(req,res)=>{
-  h = lg.gpiochipOpen(0)
-
-  lg.gpioClaimOutput(h,GREEN)
-  switch(swtch){
-    case 0:
-      swtch = 1
-      break 
-    case 1:
-      swtch = 0 
-      break 
+app.post('/green',(req,res)=>{
+  console.log(req.body)
+  switch(req.body["green"]){
+    case true:
+      turnItOn(ledPinout['green'])
+      status['green'] = !status['green']
+      break
+    case false:
+      turnItOff(ledPinout['green'])
+      status["green"] = !status['green']
   }
-  if(swtch===0){
-    lg.gpioWrite(h, GREEN, 0)
-    for(let [key,value] of Object.entries(counters)){
-      count = value
-    }
-    console.log(count)
-    res.send(`<b>sam visitor:${count}; LED off</b>`)
-
-  } else {
-    lg.gpioWrite(h, GREEN, 1)
-    for(let [key,value] of Object.entries(counters)){
-      count = value
-    }
-    console.log(count)
-    res.status(200).send(`<b>sam visitor:${count}; LED on</b>`)
-  }
-
-  lg.gpioFree(h,GREEN)
-  lg.gpiochipClose(h)
 })
+
 
 app.get('/pinstatus',(req,res)=>{
+  console.log("TOUCHED")
+  // res.send(status)
   h = lg.gpiochipOpen(0)
-  let statusObj = iterLED(h)
+  let statusObj = iterLED(h, status)
   res.send(statusObj)
-   
 })
-let ledPinout = {
-  "red":23,
-  "yellow": 21,
-  "green": 20
-}
-let ledStatus = {
-  "red": false, 
-  "yellow": false,
-  "green": false
-}
 
-const iterLED = (handle) =>{
-  if(handle){
-    for(let i of Object.keys(ledPinout)){
-      lg.gpioClaimOutput(handle,ledPinout[i])
-      // lg.gpioRead(handle,ledPinout[i])
-      switch(lg.gpioRead(handle,ledPinout[i])){
-        case 1:
-          ledStatus[i] = true;
-          break;
-        case 0:
-          ledStatus[i] = false;
-          break;
-      }
-      lg.gpioFree(handle,ledPinout[i])
-    }
-    lg.gpiochipClose(handle)
-    return ledStatus
-  }
-}
 
 app.listen(port,()=>{
   console.log(`todo esta bien; usando puerto:${port}`)
---
+++ src/utils.js
@@ -0,0 +1,42 @@
+const lg = require('lgpio')
+
+exports.ledPinout = {
+  "red":23,
+  "yellow": 21,
+  "green": 20
+}
+
+exports.iterLED = (handle,status) =>{
+  if(handle){
+    for(let i of Object.keys(ledPinout)){
+      lg.gpioClaimOutput(handle,ledPinout[i])
+      switch(lg.gpioRead(handle,ledPinout[i])){
+        case 1:
+          status[i] = true;
+          break;
+        case 0:
+          status[i] = false;
+          break;
+      }
+      lg.gpioFree(handle,ledPinout[i])
+    }
+    lg.gpiochipClose(handle)
+    return this.status
+  }
+}
+
+exports.turnItOn =(pin)=>{
+  h = lg.gpiochipOpen(0)
+  lg.gpioClaimOutput(h,pin)
+  lg.gpioWrite(h, pin, 1)
+  lg.gpioFree(h,pin)
+  lg.gpiochipClose(h)
+};
+
+exports.turnItOff=(pin)=>{
+  h = lg.gpiochipOpen(0)
+  lg.gpioClaimOutput(h,pin)
+  lg.gpioWrite(h, pin, 0)
+  lg.gpioFree(h,pin)
+  lg.gpiochipClose(h)
+}
