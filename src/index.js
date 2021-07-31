4895fb0713f80f527e57dd2e29b6e7c11c3414d2
Kris! <42949874+krstphrrr@users.noreply.github.com>
2021-07-30 23:45:17 -0600

    grrr

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const lg = require('lgpio')
 const visitCount = require('express-visitor-counter')
-const {ledPinout, iterLED, turnItOn, turnItOff} = require('./utils')
+const {ledPinout, turnItOn, turnItOff} = require('./utils')
 const app = express()
 const port = 3000
 
